cbc2e97e0ff4b42efb51eaa72a2ca93a3888eac4
Budarin Sergey <budarin_s@mail.ru>
2022-05-28 21:44:32 +0300

    get photo dict

--- main.py
+++ main.py
@@ -24,8 +24,25 @@ class VKphoto:
 token = 'db18a28ddb18a28ddb18a28d01db646e53ddb18db18a28db9891ce8a83d57616688a449'
 vk_photos = VKphoto(token, '5.131')
 
-pprint(vk_photos.get_photos())
+#pprint(vk_photos.get_photos()['response']['items'])
+photos_list = vk_photos.get_photos()['response']['items']
+#pprint(photos_list)
+#pd.DataFrame(photos_list)
+neded_photo = {}
+for photo in photos_list:
+    key = photo['comments']['count']
+    value = photo['sizes'][-1]['url']
+    if key not in neded_photo.keys() and value not in neded_photo.values():
+        neded_photo[key] = value
+    elif value  in neded_photo.values():
+        pass
+    else:
+        neded_photo[key + photo['date']] = value
+pprint(neded_photo)
 
 
 
 
+
+
+#if __name__ = '__main__':
